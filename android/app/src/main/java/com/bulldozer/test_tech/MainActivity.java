682e013ffed05dee1953b62185150d7723752561
waelmessaadi16 <waelmessaadi16@gmail.com>
2022-08-22 21:32:14 +0100

    initiale commit

---
+++ android/app/src/main/java/com/bulldozer/test_tech/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bulldozer.test_tech;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/java/com/bulldozer/test_technique_bulldozer/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bulldozer.test_technique_bulldozer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
